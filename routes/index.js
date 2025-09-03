cd025eaed78381ad8ad854d0155962cea33c4cd1
neo06060 <saulcgaos@gmail.com>
2025-09-03 18:28:54 +0200

    a

--- routes/index.js
+++ routes/index.js
@@ -1,6 +1,6 @@
 const router = require('express').Router();
 
-//router.get('/', (req, res) => { res.send('Hello world');});
+router.get('/', (req, res) => { res.send('Hello world');});
 
 router.use('/users', require('./users'));
 
