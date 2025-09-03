753596bc0dae2db7e38fc83111191e92c8ea1da2
neo06060 <saulcgaos@gmail.com>
2025-09-03 17:47:33 +0200

    a

--- server.js
+++ server.js
@@ -1,6 +1,6 @@
 const express = require('express');
 
-const mongodb = require('data/database');
+const mongodb = require('./data/database');
 const app = express()
 
 const port = process.env.PORT || 3000;
